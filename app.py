a1bc1cdb2575011954236da47fd89a584cb74926
atharvap995-sys <atharvap995@gmail.com>
2026-03-16 22:46:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from Flask import Flask
+from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
- return "Hello from Docker Python Application"
+    return "Hello from Docker Python Application"
+
 if __name__ == "__main__":
- app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
